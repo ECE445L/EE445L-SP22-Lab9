1b88556dfd4d4509396d0c368eb4625297cf3eb6
jared01m <jared01m@utexas.edu>
2022-11-05 17:05:48 -0500

    fix fft abstractions

--- sw/lib/decoder/fft/fft.c
+++ sw/lib/decoder/fft/fft.c
@@ -4,7 +4,7 @@
  * @brief Implementation of the abstracted out fast fourier transform functions
  * from the stm32 assembly files
  * @version 0.1
- * @date 2022-10-31
+ * @date 2022-11-05
  * 
  * @copyright Copyright (c) 2022
  * 
@@ -20,18 +20,18 @@
  *  inc/cr4_fft_1024_stm32.s
  */
 
-extern void cr4_fft_64_stm32(uint16_t in[], uint16_t out[], uint16_t n);
-extern void cr4_fft_256_stm32(uint16_t in[], uint16_t out[], uint16_t n);
-extern void cr4_fft_1024_stm32(uint16_t in[], uint16_t out[], uint16_t n);
+extern void cr4_fft_64_stm32(uint16_t out[], uint16_t in[], uint16_t n);
+extern void cr4_fft_256_stm32(uint16_t out[], uint16_t in[], uint16_t n);
+extern void cr4_fft_1024_stm32(uint16_t out[], uint16_t in[], uint16_t n);
 
-void fft_64(uint16_t in[], uint16_t out[]) {
-    cr4_fft_64_stm32(in, out, 64);
+void fft_64(uint16_t out[], uint16_t in[]) {
+    cr4_fft_64_stm32(out, in, 64);
 }
 
-void fft_256(uint16_t in[], uint16_t out[]) {
-    cr4_fft_64_stm32(in, out, 256);
+void fft_256(uint16_t out[], uint16_t in[]) {
+    cr4_fft_64_stm32(out, in, 256);
 }
 
-void fft_1024(uint16_t in[], uint16_t out[]) {
-    cr4_fft_64_stm32(in, out, 1024);
+void fft_1024(uint16_t out[], uint16_t in[]) {
+    cr4_fft_64_stm32(out, in, 1024);
 }
--- sw/lib/decoder/fft/fft.h
+++ sw/lib/decoder/fft/fft.h
@@ -3,7 +3,7 @@
  * @author Jared McArthur
  * @brief Header file to abstract out stm32 fft functions/assembly files
  * @version 0.1
- * @date 2022-10-31
+ * @date 2022-11-05
  * 
  * @copyright Copyright (c) 2022
  * 
@@ -16,23 +16,23 @@
 /**
  * @brief Fast fourier transform with 64 elements
  * 
- * @param in    input buffer (64 elements)
  * @param out   output buffer (64 elements)
+ * @param in    input buffer (64 elements)
  */
-void fft_64(uint16_t in[], uint16_t out[]);
+void fft_64(uint16_t out[], uint16_t in[]);
 
 /**
  * @brief Fast fourier transform with 256 elements.
  * 
- * @param in    input buffer (256 elements)
  * @param out   outpur buffer (256 elements)
+ * @param in    input buffer (256 elements)
  */
-void fft_256(uint16_t in[], uint16_t out[]);
+void fft_256(uint16_t out[], uint16_t in[]);
 
 /**
  * @brief Fast fourier transform with 1024 elements.
  * 
- * @param in    input buffer (1024 elements)
  * @param out   output buffer (1024 elements)
+ * @param in    input buffer (1024 elements)
  */
-void fft_1024(uint16_t in[], uint16_t out[]);
+void fft_1024(uint16_t out[], uint16_t in[]);
